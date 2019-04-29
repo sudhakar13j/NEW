79234fa3798be2ac36553f7c7bde860e5647a33c
Sudhakar13j <sudhakar.anandan@ravsoftsolutions.com>
2019-04-29 02:27:59 -0700

    mail

--- vars/StartExecution.groovy
+++ vars/StartExecution.groovy
@@ -8,5 +8,11 @@ def call()
    SampleObj.testgroovy()
    echo "Welcome to Jenkins"
   }
+  stage('SendEmail')
+  {
+   def SampleEmail = new Scripts.SendEmail()
+   SampleEmail.SendEmail("sudhakar.anandan@ravsoftsolutions.com","deepak.kumar@ravsoftsolutions.com","demo_mail")
+   echo "Mail Sent Successfully"
+  }
  }
 }
