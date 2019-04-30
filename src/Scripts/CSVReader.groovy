836a40ba7e922fbe4e6a3b8f38f26904eaa79d3e
Sudhakar13j <sudhakar.anandan@ravsoftsolutions.com>
2019-04-30 03:02:32 -0700

    commit

--- src/Scripts/CSVReader.groovy
+++ src/Scripts/CSVReader.groovy
@@ -2,7 +2,7 @@ package Scripts
 int ReadCSVFile(def field)
 {
  echo "ReadCSVFile.groovy file executed"
- def file = new File("C:\\Newrepo\\vars\\Config.csv");
+ def file = new File("C:/Newrepo/vars/Config.csv");
  String[] lines = file.text.split('\n')
  for(int i =0; i<lines.size(); i++)
  {
