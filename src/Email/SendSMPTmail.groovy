5b63bfbedda545d768663ff12d47a2b01d055e97
Sudhakar13j <sudhakar.anandan@ravsoftsolutions.com>
2019-04-30 02:16:49 -0700

    mail

--- src/Email/SendSMPTmail.groovy
+++ src/Email/SendSMPTmail.groovy
@@ -8,47 +8,46 @@ import javax.mail.internet.*
 
 def sendMail(receivers, subject, text) {
 
-println "-->SMTP mail configure"
 // Variables
 def SystemAdminMailAddress = 'admin@merge.com'
-def SMTPPort = 25
+def SMTPPort = '25'
 def SMTPHost = 'mail.products.network.internal'
 
 // Constants
 def instance = Jenkins.getInstance()
 def mailServer = instance.getDescriptor("hudson.tasks.Mailer")
 def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
-def extmailServer = instance.getDescriptor("hudson.plugins.emailext.ExtendedEmailPublisher")
-
+//def extmailServer = instance.getDescriptor("hudson.plugins.emailext.ExtendedEmailPublisher")
+   
         //Jenkins Location
         println "--> Configuring JenkinsLocation"
         jenkinsLocationConfiguration.setAdminAddress(SystemAdminMailAddress)
         jenkinsLocationConfiguration.save()
-
         //E-mail Server
-        //mailServer.setSmtpAuth(SMTPUser, SMTPPassword)
         mailServer.setSmtpHost(SMTPHost)
         mailServer.setSmtpPort(SMTPPort)
         mailServer.setCharset("UTF-8")
+        // Save the state
+        instance.save() 
+    
+    Properties props = new Properties();
+    //props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.host", SMTPHost);
+    //props.put("mail.smtp.user", SMTPUser);
+    props.put("mail.smtp.port", SMTPPort);
+    //props.put("mail.smtp.password", SMTPPassword);
 
-    // Save the state
-    instance.save()
-	
-	//Send Email
-	Properties props = System.getProperties()
-    props.put("mail.smtp.host", SMTPHost)
-    Session session = Session.getDefaultInstance(props, null)
-
-    MimeMessage message = new MimeMessage(session)
-    message.setFrom(new InternetAddress(SystemAdminMailAddress))
-    receivers.split(',').each {
-        message.addRecipient(Message.RecipientType.TO, new InternetAddress(it))
-    }
-    message.setSubject(subject)
-    message.setText(text)
-
-    println 'Sending mail to ' + receivers + '.'
-    Transport.send(message)
-    println 'Mail sent.'
+    Session session = Session.getInstance(props, null);
+   
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(SystemAdminMailAddress));
+        //InternetAddress addressTo = new InternetAddress[receivers];
+       // addressTo[i] = new InternetAddress(addressTo);
+        message.setRecipients(Message.RecipientType.TO, receivers);
+        message.setSubject(subject);
+        message.setText(text);
+        println 'Sending mail to ' + receivers + '.'
+        Transport.send(message);
+        println 'Mail sent.'
     
 	}
