540091d079e788681eeafb29edfdbae508efe000
Sudhakar13j <sudhakar.anandan@ravsoftsolutions.com>
2019-04-30 03:07:00 -0700

    smtp

--- src/Email/SendSMPTmail.groovy
+++ src/Email/SendSMPTmail.groovy
@@ -8,28 +8,27 @@ import javax.mail.internet.*
 
 def sendMail(receivers, subject, text)
 {
-	def SystemAdminMailAddress = 'admin@merge.com'
-	def SMTPPort = '25'
-	def SMTPHost = 'mail.products.network.internal'
+	// Variables
+def SystemAdminMailAddress = 'admin@merge.com'
+def SMTPPort = '25'
+def SMTPHost = 'mail.products.network.internal'
 
-	// Constants
-	def instance = Jenkins.getInstance()
-	def mailServer = instance.getDescriptor("hudson.tasks.Mailer")
-	def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
-	//def extmailServer = instance.getDescriptor("hudson.plugins.emailext.ExtendedEmailPublisher")
+// Constants
+def instance = Jenkins.getInstance()
+def mailServer = instance.getDescriptor("hudson.tasks.Mailer")
+def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
+//def extmailServer = instance.getDescriptor("hudson.plugins.emailext.ExtendedEmailPublisher")
    
-	//Jenkins Location
-	println "--> Configuring JenkinsLocation"
-	jenkinsLocationConfiguration.setAdminAddress(SystemAdminMailAddress)
-	jenkinsLocationConfiguration.save()
-	
-	//E-mail Server
-	mailServer.setSmtpHost(SMTPHost)
-	mailServer.setSmtpPort(SMTPPort)
-	mailServer.setCharset("UTF-8")
-	
-	// Save the state
-	instance.save() 
+        //Jenkins Location
+        println "--> Configuring JenkinsLocation"
+        jenkinsLocationConfiguration.setAdminAddress(SystemAdminMailAddress)
+        jenkinsLocationConfiguration.save()
+        //E-mail Server
+        mailServer.setSmtpHost(SMTPHost)
+        mailServer.setSmtpPort(SMTPPort)
+        mailServer.setCharset("UTF-8")
+        // Save the state
+        instance.save() 
     
     Properties props = new Properties();
     //props.put("mail.smtp.auth", "true");
@@ -40,12 +39,14 @@ def sendMail(receivers, subject, text)
 
     Session session = Session.getInstance(props, null);
    
-	Message message = new MimeMessage(session);
-	message.setFrom(new InternetAddress(SystemAdminMailAddress));
-	message.setRecipients(Message.RecipientType.TO, receivers);
-	message.setSubject(subject);
-	message.setText(text);
-	println 'Sending mail to ' + receivers + '.'
-	Transport.send(message);
-	println 'Mail sent.'
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(SystemAdminMailAddress));
+        //InternetAddress addressTo = new InternetAddress[receivers];
+       // addressTo[i] = new InternetAddress(addressTo);
+        message.setRecipients(Message.RecipientType.TO, receivers);
+        message.setSubject(subject);
+        message.setText(text);
+        println 'Sending mail to ' + receivers + '.'
+        Transport.send(message);
+        println 'Mail sent.'
 	}
