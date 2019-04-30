d010566bc683b7d2f694d3b3a4ace764cb82c192
Sudhakar13j <sudhakar.anandan@ravsoftsolutions.com>
2019-04-30 03:22:36 -0700

    csvb

--- src/Scripts/CSVReader.groovy
+++ src/Scripts/CSVReader.groovy
@@ -2,11 +2,25 @@ package Scripts
 int ReadCSVFile(def field)
 {
  echo "ReadCSVFile.groovy file executed"
- def file = new File("C:/Newrepo/vars/Config.csv");
+ int row = 0;
+ int col = 0;
+ def file = new File("C:/Newrepo/vars/Config.csv")
  String[] lines = file.text.split('\n')
  for(int i =0; i<lines.size(); i++)
  {
- println "$lines[i]"
- String[] rowvalues = row.split(',');
+  println "$lines[i]"
+  String[] rowvalues = lines.split(',')
+  for(int j=0;i<rowvalues.size();j++)
+  {
+   if(rowvalues[j] == field)
+   {
+    row = i+1;
+	col = j;
+	break;
+   }
+  }
  }
+ rowvalues = lines[row].split(',')
+ println "owvalues[col]"
+ return rowvalues[col]
 }
