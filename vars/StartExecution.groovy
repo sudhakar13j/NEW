9a429a9c6d9e072dcf9046061c9588ff84b8b672
Sudhakar13j <sudhakar.anandan@ravsoftsolutions.com>
2019-05-02 05:24:42 -0700

    csv

--- vars/StartExecution.groovy
+++ vars/StartExecution.groovy
@@ -21,7 +21,7 @@ def call()
 	println "$Subject"
 	println "$Body"
 	println "$Attachment"
-	SampleEmail.sendMail(Receiver,Subject,Body)
+	SampleEmail.sendMail(Receiver,Subject,Body,Attachment)
 	echo "Mail Sent Successfully"
   }
   stage('CSVReader')
