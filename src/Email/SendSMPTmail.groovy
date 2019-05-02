d95070dc46aaa1b1c61a022abb84207bec4d481c
Sudhakar13j <sudhakar.anandan@ravsoftsolutions.com>
2019-05-02 05:14:06 -0700

    csv

--- src/Email/SendSMPTmail.groovy
+++ src/Email/SendSMPTmail.groovy
@@ -8,34 +8,30 @@ import javax.mail.internet.*
 
 def sendMail(receivers, subject, text, attachment)
 {
-def EmailConfig = new Scripts.CSVReader()
-	// Variables
-/*def SystemAdminMailAddress = 'admin@merge.com'
-def SMTPPort = '25'
-def SMTPHost = 'mail.products.network.internal'*/
-def SystemAdminMailAddress = EmailConfig.ReadCSVFile("SMTPAdmin");
-def SMTPPort = EmailConfig.ReadCSVFile("SMTPPort");
-def SMTPHost = EmailConfig.ReadCSVFile("SMTPHost");
-println "SystemAdminMailAddress: $SystemAdminMailAddress"
-println "SMTPPort: $SMTPPort"
-println "SMTPHost: $SMTPHost"
+	def EmailConfig = new Scripts.CSVReader()
+	def SystemAdminMailAddress = EmailConfig.ReadCSVFile("SMTPAdmin");
+	def SMTPPort = EmailConfig.ReadCSVFile("SMTPPort");
+	def SMTPHost = EmailConfig.ReadCSVFile("SMTPHost");
+	println "SystemAdminMailAddress: $SystemAdminMailAddress"
+	println "SMTPPort: $SMTPPort"
+	println "SMTPHost: $SMTPHost"
 
-// Constants
-def instance = Jenkins.getInstance()
-def mailServer = instance.getDescriptor("hudson.tasks.Mailer")
-def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
-//def extmailServer = instance.getDescriptor("hudson.plugins.emailext.ExtendedEmailPublisher")
+	// Constants
+	def instance = Jenkins.getInstance()
+	def mailServer = instance.getDescriptor("hudson.tasks.Mailer")
+	def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
+	//def extmailServer = instance.getDescriptor("hudson.plugins.emailext.ExtendedEmailPublisher")
    
-        //Jenkins Location
+    //Jenkins Location
         
-        jenkinsLocationConfiguration.setAdminAddress(SystemAdminMailAddress)
-        jenkinsLocationConfiguration.save()
-        //E-mail Server
-        mailServer.setSmtpHost(SMTPHost)
-        mailServer.setSmtpPort(SMTPPort)
-        mailServer.setCharset("UTF-8")
-        // Save the state
-        instance.save() 
+	jenkinsLocationConfiguration.setAdminAddress(SystemAdminMailAddress)
+	jenkinsLocationConfiguration.save()
+	//E-mail Server
+	mailServer.setSmtpHost(SMTPHost)
+	mailServer.setSmtpPort(SMTPPort)
+	mailServer.setCharset("UTF-8")
+	// Save the state
+	instance.save() 
     
     Properties props = new Properties();
     //props.put("mail.smtp.auth", "true");
@@ -43,23 +39,37 @@ def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
     //props.put("mail.smtp.user", SMTPUser);
     props.put("mail.smtp.port", SMTPPort);
     //props.put("mail.smtp.password", SMTPPassword);
-println "--> Configuring JenkinsLocation"
+	println "--> Configuring JenkinsLocation"
      Session session = Session.getInstance(props, null);
         // Create a default MimeMessage object.
-        Message msg = new MimeMessage(session);
-        msg.setFrom(new InternetAddress(SystemAdminMailAddress))
-        msg.setRecipients(Message.RecipientType.TO, receivers);
-        msg.setSubject(subject)
-        BodyPart messageBodyPart = new MimeBodyPart()
-        messageBodyPart.setContent(text,"text/html")
-        Multipart multipart = new MimeMultipart()
-        multipart.addBodyPart(messageBodyPart)
-
-        messageBodyPart = new MimeBodyPart()
-        messageBodyPart.attachFile(attachment)  
-        multipart.addBodyPart(messageBodyPart)
-		println "--> Attachement added"
-        // Send the complete message parts
-        msg.setContent(multipart)
-        Transport.send(msg)     
+	if (attachment == null || attachment.empty)
+	{
+		Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(SystemAdminMailAddress));
+        message.setRecipients(Message.RecipientType.TO, receivers);
+        message.setSubject(subject);
+        message.setText(text);
+        println 'Sending mail to ' + receivers + '.'
+        Transport.send(message);
+        println 'Mail sent.'
 	}
+	else
+	{
+	Message msg = new MimeMessage(session);
+	msg.setFrom(new InternetAddress(SystemAdminMailAddress))
+	msg.setRecipients(Message.RecipientType.TO, receivers);
+	msg.setSubject(subject)
+	BodyPart messageBodyPart = new MimeBodyPart()
+	messageBodyPart.setContent(text,"text/html")
+	Multipart multipart = new MimeMultipart()
+	multipart.addBodyPart(messageBodyPart)
+
+	messageBodyPart = new MimeBodyPart()
+	messageBodyPart.attachFile(attachment)  
+	multipart.addBodyPart(messageBodyPart)
+	println "--> Attachement added"
+	// Send the complete message parts
+	msg.setContent(multipart)
+	Transport.send(msg)   
+	}
+}
