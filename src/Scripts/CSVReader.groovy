35c5650b11c73fe5dd351a85b0018396155ca97c
Sudhakar13j <sudhakar.anandan@ravsoftsolutions.com>
2019-05-02 01:53:08 -0700

    csv

--- src/Scripts/CSVReader.groovy
+++ src/Scripts/CSVReader.groovy
@@ -8,22 +8,10 @@ int ReadCSVFile(def field)
  int j = 0;
  def file = new File("C:/Newrepo/vars/Config.csv")
  String[] lines = file.text.split('\n')
- println "ttorow:$lines.size()"
  for(i =0; i<lines.size(); i++)
  {
+  println "values:$rowvalues[i]\n"
   String[] rowvalues = lines[i].split(',')
-  println "colcount:$rowvalues.size()"
-  for(j=0;j<rowvalues.size();j++)
-  {
-   if(rowvalues[j].contains(field))
-   {
-    row = i+1;
-	col = j;
-	break;
-   }
-  }
  }
- String[] values = lines[row].split(',')
- println "Step2:$values[col]"
- return rowvalues[col]
+ return row
 } 
