d90d36d0c4d624cc5114ffa29e2d2fa177a32de4
Sudhakar13j <sudhakar.anandan@ravsoftsolutions.com>
2019-05-02 03:08:26 -0700

    csv

--- vars/StartExecution.groovy
+++ vars/StartExecution.groovy
@@ -8,19 +8,21 @@ def call()
    SampleObj.testgroovy()
    echo "Welcome to Jenkins"
   }*/
-  stage('SendSMTP')
+  /*stage('SendSMTP')
   {
 	echo "SendSMTP file started"
 	def SampleEmail = new Email.SendSMPTmail()
 	SampleEmail.sendMail("sudhakar.anandan@ravsoftsolutions.com","demo_mail","SMTP Mail received","C:\\Issuedetails.txt")
 	echo "Mail Sent Successfully"
-  }
+  }*/
   stage('CSVReader')
   {
    echo "CSVReader file started"
    def csv = new Scripts.CSVReader()
-   def input = csv.ReadCSVFile("SMTPHost")
-   println "CSBInpuit:$input"
+   def input1 = csv.ReadCSVFile("ServerExecutionFile")
+   println "Input1:$input1"
+   def input2 = csv.ReadCSVFile("BuildUrl")
+   println "Input2:$input1"
   }
  }
 }
