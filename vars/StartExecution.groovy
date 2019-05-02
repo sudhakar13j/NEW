166092b174db7d580fec1cbd7f7d4ef0b5cd5421
Sudhakar13j <sudhakar.anandan@ravsoftsolutions.com>
2019-05-02 03:47:58 -0700

    csv

--- vars/StartExecution.groovy
+++ vars/StartExecution.groovy
@@ -21,7 +21,7 @@ def call()
    def csv = new Scripts.CSVReader()
    def input1 = csv.ReadCSVFile("SMTPReceiver")
    println "Input1:$input1"
-   def input2 = csv.ReadCSVFile("BuildUrl")
+   def input2 = csv.ReadCSVFile("SMTPHost")
    println "Input2:$input2"
   }
  }
