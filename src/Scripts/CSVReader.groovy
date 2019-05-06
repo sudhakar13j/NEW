f4ded8b7f22f9869ebd07800e13a07754a2a5874
Sudhakar13j <sudhakar.anandan@ravsoftsolutions.com>
2019-05-05 23:36:35 -0700

    csv

--- src/Scripts/CSVReader.groovy
+++ src/Scripts/CSVReader.groovy
@@ -1,67 +1,34 @@
 package Scripts
-List ReadCSVFile(String header)
-	{
-		println "List ReadCSVFile.groovy file executed"
-		int row = 0
-		int col = 0
-		int i = 0
-		int j = 0
-		int k = 0
-		int rowCount = 0
-		int colCount = 0
-		def file = new File("C:/Newrepo/vars/ConfigParam.csv")
-		List Arrayvalues = new ArrayList()
-		String[] lines = file.text.split('\n')
-		rowCount = lines.size();
-		for(i =0; i<rowCount; i++)
-		{
-			if(lines[i].contains(header))
-			{
-				row = i+2
-				break
-			}
-		}
-		for(j=row;j<rowCount;j++)
-		{
-			if(lines[j].contains("@Stage") || lines[j] == "<EOF>" || lines[j] == '\r')
-				break
-			else
-				Arrayvalues.add(lines[j])
-		}
-		int n = Arrayvalues.size()
-		for(k=0;k<n;k++)
-		{
-			Arrayvalues[k] = Arrayvalues[k].replace('{', '')
-			Arrayvalues[k] = Arrayvalues[k].replace('}', '')
-			Arrayvalues[k] = Arrayvalues[k].replace(' ', '')
-		}
-		return Arrayvalues
-	}
-	String[] ReadCSVFile(String header, String field)
-	{
-		println "String ReadCSVFile.groovy file executed"
-		field = null//Declared that variable to use same method name with different return type
-		int row = 0
-		int col = 0
-		int i = 0
-		int j = 0
-		int k = 0
-		int rowCount = 0
-		int colCount = 0
-		def file = new File("C:/Newrepo/vars/ConfigParam.csv")
-		String[] lines = file.text.split('\n')
-		rowCount = lines.size();
-		for(i =0; i<rowCount; i++)
-		{
-			if(lines[i].contains(header))
-			{
-				row = i+2
-				break
-			}
-		}
-		lines[row] = lines[row].replace('{', '')
-		lines[row] = lines[row].replace('}', '')
-		lines[row] = lines[row].replace(' ', '')
-		String[] value = lines[row].split(',')
-		return value
-	}
+int ReadCSVFile(def field)
+{
+ echo "ReadCSVFile.groovy file executed"
+ int row = 0
+ int col = 0
+ int i = 0
+ int j = 0
+ int rowCount = 0
+ int colCount = 0
+ def file = new File("C:/Newrepo/vars/Config.csv")
+ String[] lines = file.text.split('\n')
+ rowCount = lines.size();
+
+ for(i =0; i<rowCount; i++)
+ {
+  String[] rowvalues = lines[i].split(',')
+  colCount = rowvalues.size();
+ 
+  for(j=0;j<colCount;j++)
+  {
+   if(rowvalues[j].equals(field))
+   {
+    row = i+1
+	col = j
+	break;
+   }
+  }
+ }
+ println("Found place:$row and $col")
+ String[] values = lines[row].split(',')
+ String csvvalue = values[col]
+ return csvvalue
+}
