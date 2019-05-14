c56893b82788fc2e603d96f1953ad483f7e94e6f
Sudhakar13j <sudhakar.anandan@ravsoftsolutions.com>
2019-05-14 02:45:23 -0700

    fil

--- vars/Start.groovy
+++ vars/Start.groovy
@@ -16,8 +16,9 @@ def call()
   }
   stage('poweroff')
   {
-   vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: 'ica-tst21-ws12'], serverName: 'NEPTUNE'
-	echo "${VmName} is Switched ON"
+   //vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: 'ica-tst21-ws12'], serverName: 'NEPTUNE'
+   vSphere buildStep: [$class: 'PowerOn', timeoutInSeconds: 260, vm: 'ica-tst21-ws12'], serverName: 'NEPTUNE'
+	echo "ica-tst21-ws12 is Switched ON"
   }
  }
 }
