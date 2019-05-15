0b64bb48506afee38160b374ce4eaa5635a4b171
Sudhakar13j <sudhakar.anandan@ravsoftsolutions.com>
2019-05-15 03:57:35 -0700

    csv

--- src/Scripts/SendEmail.groovy
+++ src/Scripts/SendEmail.groovy
@@ -21,7 +21,7 @@ println "receivers: $receivers"
 println "subject: $subject"
 println "text: $text"
 println "attachment: $attachment"
-EmailNotification(SystemAdminMailAddress,SMTPPort,SMTPHost,receivers, subject, text)
+EmailNotification(SystemAdminMailAddress,SMTPPort,SMTPHost,receivers, subject, text, attachment)
 }
 def EmailNotification(SystemAdminMailAddress,SMTPPort,SMTPHost,receivers, subject, text, String attachment = null)
 {
